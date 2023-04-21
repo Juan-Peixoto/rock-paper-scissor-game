0ff911379e1223b0c0667239e323f7a84bf66271
Juan Peixoto <juanpeixoto1998@gmail.com>
2023-04-21 11:08:10 -0300

    Created pseudo code

---
+++ main.js
@@ -0,0 +1,10 @@
+// player needs to select an option
+// opponent automatically selects an option
+// then compare the options
+// show the winner
+// ask for play again
+//  if yes
+//      sum interates the total of wins of the winner
+//      then restart the game
+//  else
+//      restart the game
