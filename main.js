d4ee16ab424dc33bcf0db0dfdffc4cabc29a6a6b
Juan Peixoto <juanpeixoto1998@gmail.com>
2023-04-21 11:09:46 -0300

    Fixed pseudo code

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 // player needs to select an option
-// opponent automatically selects an option
+// opponent automatically selects an option too
 // then compare the options
 // show the winner
 // ask for play again
 //  if yes
-//      sum interates the total of wins of the winner
+//      sum and interates the total of wins of the winner
+//      show it in the botton
 //      then restart the game
 //  else
 //      restart the game
